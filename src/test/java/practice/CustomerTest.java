e113b23504b9182e2ea5d3e23456b7e61269e370
admin <pranalikharpude01@gmail.com>
2022-09-20 18:47:39 +0530

    Commit Eng 1

---
+++ src/test/java/practice/CustomerTest.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void testingTest()
+	{
+		System.out.println("This  is the Project.... ");
+	}
+	
+	
+}
